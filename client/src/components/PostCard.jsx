a442c405e5377c629273fc67f8d88a2790711783
wolfee <wolfee.dm@gmail.com>
2021-10-10 01:14:37 +0200

    minor design change

--- client/src/components/PostCard.jsx
+++ client/src/components/PostCard.jsx
@@ -2,26 +2,44 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-    margin: 10px 10px 15px 10px;
+    display: flex;
+    flex-direction: column;
 `;
 
-const Image = styled.img``;
+const Info = styled.div`
+    margin: 10px 5px 10px 5px;
+    padding: 5px;
+    flex: 1;
+    background-color: rgba(255, 255, 255, 0.4);
+`;
 
-const Title = styled.h4``;
+const Image = styled.img`
+    width: 100%;
+    height: auto;
+`;
 
-const Author = styled.h5``;
+const Title = styled.h4`
+    margin-top: 5px;
+`;
+
+const Author = styled.h5`
+    margin-top: 5px;
+`;
 
 const Short = styled.p`
+    margin-top: 5px;
     font-size: 14px;
 `;
 
 const PostCard = ({ post }) => {
   return (
     <Container>
-      <Image src={post.img} />
-      <Title>{post.title}</Title>
-      <Author>{post.author}</Author>
-      <Short>{post.short}</Short>
+      <Info>
+        <Image src={post.img} />
+        <Title>{post.title}</Title>
+        <Author>{post.author}</Author>
+        <Short>{post.short}</Short>
+      </Info>
     </Container>
   );
 };
